6ee9dec1f68fb3f4a49ae83c3a712784fea7c3c7
liangbing <liangbingbing@dankegongyu.com>
2019-07-09 14:11:25 +0800

    no.3

--- src/main/java/me/shouly/leetcode/training/medium/No_3_LengthOfLongestSubStr.java
+++ src/main/java/me/shouly/leetcode/training/medium/No_3_LengthOfLongestSubStr.java
@@ -1,5 +1,10 @@
 package me.shouly.leetcode.training.medium;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
  *
@@ -13,7 +18,9 @@ package me.shouly.leetcode.training.medium;
  *
  * <p>示例 3:
  *
- * <p>输入: "pwwkew" 输出: 3 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。   请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串.
+ * <p>输入: "pwwkew" 输出: 3 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ *
+ * <p>请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串.
  *
  * @author liangbing
  * @version v1.0
@@ -21,8 +28,47 @@ package me.shouly.leetcode.training.medium;
  */
 public class No_3_LengthOfLongestSubStr {
 
-  public int lengthOfLongestSubstring(String s) {
+  public static int lengthOfLongestSubstring(String s) {
 
-    return 0;
+    int length = s.length();
+
+    Set<Character> cache = new HashSet<>();
+
+    int i = 0;
+    int j = 0;
+    int result = 0;
+
+    while (i < length && j < length) {
+
+      if (cache.contains(s.charAt(j))) {
+        cache.remove(s.charAt(i++));
+      } else {
+        cache.add(s.charAt(j++));
+        result = Math.max(j - i, result);
+      }
+    }
+
+    return result;
+  }
+
+  public static int officialAns(String s) {
+
+    int n = s.length(), ans = 0;
+    Map<Character, Integer> map = new HashMap<>(16);
+    // try to extend the range [i, j]
+    for (int j = 0, i = 0; j < n; j++) {
+      if (map.containsKey(s.charAt(j))) {
+        i = Math.max(map.get(s.charAt(j)), i);
+      }
+      ans = Math.max(ans, j - i + 1);
+      map.put(s.charAt(j), j + 1);
+    }
+    return ans;
+  }
+
+  public static void main(String[] args) {
+    String s = "askdfjasdk";
+    System.out.println(lengthOfLongestSubstring(s));
+    System.out.println(officialAns(s));
   }
 }
